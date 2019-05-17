d778644213c7f2d19ec39c3e86bf853ba0af3a24
Gleider Costa - Getin <gleider.costa@alterosa.CONSERVO>
2019-05-17 17:17:01 -0300

    First commit

---
+++ src/graphit-json.js
@@ -0,0 +1,36 @@
+class Graphit_JSON {
+  constructor() {
+    this.db = {};
+    this.list = {};
+    this.lastId = 0;
+  }
+  new_id() { return this.lastId++; }
+
+  retrieve_val(id) { 
+    return Promise.resolve(this.db[id]); 
+  }
+
+  set_val({ id, data }) {
+    this.db[id] = data;
+    return Promise.resolve({id, data}); 
+  }
+
+  retrieve_list(from_id) { 
+    return Promise.resolve(this.list[from_id]); 
+  }
+  
+  set_list({ from_id, list }) {
+    this.list[from_id] = list;
+    return Promise.resolve(this.list[from_id]); 
+  }
+
+  remove(id) { 
+    delete this.db[id];
+    delete this.list[id];
+    return Promise.resolve(); 
+  }
+}
+
+if(module) {
+  module.exports.Graphit_JSON = Graphit_JSON;
+}
